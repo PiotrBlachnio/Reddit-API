a855b6a61a17f06f049ac35bec8ad01437552a20
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-02 09:38:43 +0100

    Create model/Comment.java

---
+++ src/main/java/com/piotrblachnio/reddit/model/Comment.java
@@ -0,0 +1,37 @@
+package com.piotrblachnio.reddit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+import java.time.Instant;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String text;
+
+    @ManyToOne(fetch = LAZY)
+    private Post post;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+
+    private Instant createdDate;
+}
