edacab76b5a808ba08e67be2253aad0aba3d5a2a
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-02 10:30:24 +0100

    Create repository/VoteRepository.java

---
+++ src/main/java/com/piotrblachnio/reddit/repository/VoteRepository.java
@@ -0,0 +1,11 @@
+package com.piotrblachnio.reddit.repository;
+
+import com.piotrblachnio.reddit.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
+}
