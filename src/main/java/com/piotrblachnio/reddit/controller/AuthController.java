a3bd5b08b1edb108c6629aae8ce22bf7c3025c5f
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-02 15:17:48 +0100

    Create service/AuthService.java

--- src/main/java/com/piotrblachnio/reddit/controller/AuthController.java
+++ src/main/java/com/piotrblachnio/reddit/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.piotrblachnio.reddit.controller;
 
+import com.piotrblachnio.reddit.dto.RegisterRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
