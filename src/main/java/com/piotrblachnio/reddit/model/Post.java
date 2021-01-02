5e2166c9f7b131e10d7a312a7cbb5c6965cf5446
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-02 09:44:40 +0100

    Create model/Post.java

---
+++ src/main/java/com/piotrblachnio/reddit/model/Post.java
@@ -0,0 +1,43 @@
+package com.piotrblachnio.reddit.model;
+
+import com.sun.istack.Nullable;
+import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long postId;
+
+    @NotBlank(message = "Post name cannot be empty")
+    private String postName;
+
+    @Nullable
+    private String url;
+
+    @Nullable
+    @Lob
+    private String description;
+
+    private Integer voteCount = 0;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Subreddit subreddit;
+
+    private Instant createdDate;
+}
