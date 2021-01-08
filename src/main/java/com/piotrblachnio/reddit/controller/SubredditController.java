88a32a2990c2f1cdbaafc10ae117521d905e7f62
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-08 19:54:53 +0100

    Create getAllSubreddits route in controller/SubredditController.java

--- src/main/java/com/piotrblachnio/reddit/controller/SubredditController.java
+++ src/main/java/com/piotrblachnio/reddit/controller/SubredditController.java
@@ -1,18 +1,34 @@
 package com.piotrblachnio.reddit.controller;
 
 import com.piotrblachnio.reddit.dto.SubredditDto;
+import com.piotrblachnio.reddit.service.SubredditService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/subreddit")
 @AllArgsConstructor
 @Slf4j
 public class SubredditController {
-    public void createSubreddit(@RequestBody SubredditDto subredditDto) {
+    private final SubredditService subredditService;
 
+    public ResponseEntity<SubredditDto> createSubreddit(@RequestBody SubredditDto subredditDto) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(subredditService.save(subredditDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SubredditDto>> getAllSubreddits() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(subredditService.getAll());
     }
 }
--- src/main/java/com/piotrblachnio/reddit/service/SubredditService.java
+++ src/main/java/com/piotrblachnio/reddit/service/SubredditService.java
@@ -6,6 +6,11 @@ import com.piotrblachnio.reddit.repository.SubredditRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @AllArgsConstructor
@@ -13,10 +18,26 @@ import org.springframework.stereotype.Service;
 public class SubredditService {
     private final SubredditRepository subredditRepository;
 
+    @Transactional
     public SubredditDto save(SubredditDto subredditDto) {
         var save = subredditRepository.save(mapSubredditDto(subredditDto));
         subredditDto.setId(save.getId());
         return subredditDto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<SubredditDto> getAll() {
+        return subredditRepository.findAll()
+                    .stream()
+                    .map(this::mapToDto)
+                    .collect(toList());
+    }
+
+    private SubredditDto mapToDto(Subreddit subreddit) {
+        return SubredditDto.builder().name(subreddit.getName())
+                .id(subreddit.getId())
+                .numberOfPosts(subreddit.getPosts().size())
+                .build();
     }
 
     private Subreddit mapSubredditDto(SubredditDto subredditDto) {
