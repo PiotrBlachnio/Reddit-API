850bac999ee76363b777421d42c5ad79a11ef635
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-14 16:17:50 +0100

    Create controller/PostController.java

--- src/main/java/com/piotrblachnio/reddit/controller/PostController.java
+++ src/main/java/com/piotrblachnio/reddit/controller/PostController.java
@@ -1,11 +1,10 @@
 package com.piotrblachnio.reddit.controller;
 
-import lombok.AllArgsConstructor;
+import com.piotrblachnio.reddit.dto.PostRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/post")
@@ -15,6 +14,27 @@ public class PostController {
 
     @PostMapping
     public ResponseEntity createPost(@RequestBody PostRequest postRequest) {
+        postService.save(postRequest);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
 
+    @GetMapping("/${id}")
+    public PostResponse getPost(@PathVariable Long id) {
+        return postService.getPost(id);
+    }
+
+    @GetMapping("/")
+    public List<PostResponse> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/by-subreddit/${id}")
+    public List<PostResponse> getPostsBySubreddit(Long id) {
+        return postSerice.getPostsBySubreddit(id);
+    }
+
+    @GetMapping("/by-user/${name}")
+    public List<PostResponse> getPostsByUsername(String username) {
+        return postService.getPostsByUsername(username);
     }
 }
