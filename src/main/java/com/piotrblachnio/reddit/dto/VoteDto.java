c9e9a7c313e707055a666c40bda3d5b8b1c11d97
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-15 13:22:43 +0100

    Create dto/VoteDto.java

---
+++ src/main/java/com/piotrblachnio/reddit/dto/VoteDto.java
@@ -0,0 +1,14 @@
+package com.piotrblachnio.reddit.dto;
+
+import com.piotrblachnio.reddit.model.VoteType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoteDto {
+    private VoteType voteType;
+    private Long postId;
+}
